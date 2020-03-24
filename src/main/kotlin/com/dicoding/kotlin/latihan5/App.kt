c3261c2d4335f71a9718bb326150aac149702224
rdfariz <rdfariz@student.telkomuniversity.ac.id>
2020-03-24 19:54:37 +0700

    latihan 5

--- src/main/kotlin/com/dicoding/kotlin/latihan5/App.kt
+++ src/main/kotlin/com/dicoding/kotlin/latihan5/App.kt
@@ -1,28 +1,53 @@
 package com.dicoding.kotlin.latihan5
 
-/* Aplikasi Penghitung sampah diwilayah tertentu */
+/* Aplikasi kasir simple */
 
 fun main() {
-    val angka: IntArray = intArrayOf(2,3,4,5)
-    println(sum(*angka))
-    println(messageLength("koko"))
-    println(2.printInt())
+//  Tambahkan 0 lagi di belakang untuk transaksi yang sukses
+    var saldoSaya = 100000
 
-    checkdata("koko"){
-        println(it)
+    var totalHarga = 0
+    var barang1 = mapOf<String, Any>("namaBarang" to "SilverQueen", "hargaBarang" to intArrayOf(20000, 30000))
+    var barang2 = mapOf<String, Any>("namaBarang" to "Pulpen", "hargaBarang" to  intArrayOf(3000, 2000))
+    var listBarang = ArrayList<Map<String, Any>>()
+    listBarang.add(barang1)
+    listBarang.add(barang2)
+
+    var arrHarga: IntArray
+    listBarang.forEachIndexed { index, barang ->
+        totalHarga += sumHarga(*barang["hargaBarang"] as IntArray)
+    }
+
+    bayar(totalBayar = saldoSaya, totalHarga = totalHarga) {
+        it?.let {
+            println("Pembayaran BERHASIL dilakukan, kembalian anda: $it")
+        } ?: println("Pembayaran GAGAL dilakukan!!, uang anda kurang")
+        printStruk(mapOf("title" to "Total Bayar", "harga" to saldoSaya), mapOf("title" to "Total harga", "harga" to totalHarga), mapOf("title" to "Kembalian", "harga" to it))
     }
 }
 
-val messageLength = { message: String -> message.length }
-fun Int.printInt(): Int {
-    return this + 2
+fun printStruk(vararg kategori: Map<String, Any?>) {
+    var message = "\nDetail Transaksi:\n"
+    kategori.forEach {
+        if (it["harga"] != null) {
+            message += it["harga"]?.printKalimat(it["title"] as String)
+        }
+    }
+    println(message)
 }
-
-//HOF
-fun checkdata(data: String, callback: (String) -> Unit) {
-    callback(data + " Armada")
+fun Any.printKalimat(kategori: String): String {
+    return "$kategori: $this\n"
 }
-
-fun sum(vararg number: Int): Int {
+fun sumHarga(vararg number: Int): Int {
     return number.sum()
+}
+fun bayar(totalHarga: Int, totalBayar: Int, kembalian: (Int?) -> Unit) {
+    totalHarga.let {
+        val _kembalian = totalBayar - it
+        if (_kembalian >= 0) {
+            kembalian(_kembalian)
+        }else {
+            kembalian(null)
+        }
+    }
 }
